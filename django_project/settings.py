d2dea23e8ba29baaa7b4c64c10778b1ebc70166a
Mario Fernández <ficcionyciencia.contact@gmail.com>
2020-05-13 20:52:20 -0300

    static settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
