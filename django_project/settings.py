41d25bec0cad4fdf5187a52b69c3bacc15215c55
Mario Fernández <ficcionyciencia.contact@gmail.com>
2020-05-14 12:38:10 -0300

    Added static styles to S3

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'my_blog/static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
