caebf214b5a4a3ef7902fad36e49d62e1b2bebc9
Mario Fernández <ficcionyciencia.contact@gmail.com>
2020-05-22 19:26:42 -0300

    responsive navbar v1

--- my_blog/views.py
+++ my_blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User, AnonymousUser
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, RedirectView
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -66,6 +67,8 @@ class PostLikeToggle(RedirectView):
                 post.likes.remove(user)
             else:
                 post.likes.add(user)
+        else:
+            messages.info(self.request, f'Accede en para interactuar con el contenido')
         return url_
 
 
